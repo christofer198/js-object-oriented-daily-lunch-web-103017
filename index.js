ac655b0f4f2681305ae893454ae92b9dbe56cfcd
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-14 15:50:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -111,10 +111,17 @@ class Employer{
   }
 
   mealTotals(){
-    let mealHash = {}
-    this.deliveries().map(each => {
-      mealHash[each.mealId] = 1
+    let allMeals = this.deliveries().map(delivery => {
+      return delivery.meal();
     });
+    let summaryObject = {};
+    allMeals.forEach(function(meal) {
+      summaryObject[meal.id] = 0;
+    });
+    allMeals.forEach(function(meal) {
+      summaryObject[meal.id] += 1;
+    });
+    return summaryObject;
 
   }
 
