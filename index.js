62a1de2915d38b637d181431f9daa189c30e256f
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-14 13:44:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,24 +72,6 @@ class Delivery{
     });
   }
 }
-// class Delivery {
-  // constructor(meal = {}, customer = {}) {
-  //   this.mealId = meal.id;
-  //   this.customerId = customer.id;
-  //   this.id = ++deliveryId;
-  //   store.deliveries.push(this);
-  // }
-//   meal() {
-//     return store.meals.find(meal => {
-//       return meal.id === this.mealId;
-//     });
-//   }
-//   customer() {
-//     return store.customers.find(customer => {
-//       return customer.id === this.customerId;
-//     });
-//   }
-// }
 
 //
 // class Employer{
